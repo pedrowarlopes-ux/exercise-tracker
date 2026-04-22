e5936b68706256cd31957f0fb7878ebd76667d5b
Pedro Lopes <pedro.war.lopes@gmail.com>
2026-04-22 20:47:30 -0300

    Compact layout: remove header, tighten spacing, 4-col types; tennis -> tênis

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'exercise-tracker-v4';
+const CACHE = 'exercise-tracker-v5';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon-180.png', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
